f27edbd9a5873a908bf6353b050e76b4f666a7a5
wiegand92 <wiegand.alex@gmail.com>
2021-10-18 19:10:21 -0400

    basic navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,25 +6,23 @@ const Navbar = () => (
     <Link to="/" className="navbar-title">
       ExerTracker
     </Link>
-    <div className="navbar-list">
-      <ul className="navbar-nav">
-        <li className="navbar-item">
-          <Link to="/" className="nav-link">
-            Exercises
-          </Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/create" className="nav-link">
-            Create Exercise Log
-          </Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/user" className="nav-link">
-            Create User
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <ul className="navbar-nav">
+      <li className="navbar-item">
+        <Link to="/" className="nav-link">
+          Exercises
+        </Link>
+      </li>
+      <li className="navbar-item">
+        <Link to="/create" className="nav-link">
+          Create Exercise Log
+        </Link>
+      </li>
+      <li className="navbar-item">
+        <Link to="/user" className="nav-link">
+          Create User
+        </Link>
+      </li>
+    </ul>
   </nav>
 );
 

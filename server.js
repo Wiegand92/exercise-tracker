f0cd028a61c438b38cda3e30df95958e4a4cb02d
wiegand92 <wiegand.alex@gmail.com>
2021-10-18 00:02:46 -0400

    add boilerplate for mongoose connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -12,6 +13,14 @@ const PORT = process.env.PORT || 4200;
 app.use(cors());
 app.use(express.json());
 
+//Mongoose Connection//
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log(`MongoDB database connection established successfully`);
+});
 //Serve static files//
 
 app.use(express.static(__dirname + '/public'));
